ec8e9765507252bf500197179fe35cf0da95b1c4
Jussi Saarivirta <jusasi@gmail.com>
2022-01-30 14:23:20 +0200

    Minimal tool to inspect quad database files.

---
+++ src/WatneyAstrometry.QuadDatabaseInspector/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using WatneyAstrometry.Core.QuadDb;
+
+namespace WatneyAstrometry.QuadDatabaseInspector
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var filename = args.Last();
+
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("File does not exist");
+                return;
+            }
+
+            var cellFile = new QuadDatabaseCellFile(filename);
+            var d = cellFile.FileDescriptor;
+
+            Console.WriteLine($"Cell id: {d.CellId}");
+            Console.WriteLine($"Number of passes: {d.Passes.Length}");
+            Console.WriteLine($"Pass quad densities: {string.Join(", ", d.Passes.Select(x => x.QuadsPerSqDeg.ToString(CultureInfo.InvariantCulture)))}");
+        }
+    }
+}
