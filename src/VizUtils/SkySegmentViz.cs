516b281e3b08aab3ab303af576c0be0e24ec918d
Koen van Leeuwen <koen@logiqs.nl>
2026-03-02 15:18:40 +0100

    bump Imagesharp.Drawing so the tests run again
    (and fail due to missing testdata)

--- src/VizUtils/SkySegmentViz.cs
+++ src/VizUtils/SkySegmentViz.cs
@@ -18,11 +18,10 @@ namespace VizUtils
             var imageWidth = pixelsPerDeg * 360;
             var imageHeight = pixelsPerDeg * 180;
 
-            var brush = new SolidBrush(Color.Black);
             var image = new Image<Rgba32>(imageWidth, imageHeight);
             image.Mutate(ctx =>
             {
-                ctx.Fill(new GraphicsOptions(), Color.White);
+                ctx.Fill(Color.White);
                 foreach (var cell in allCells)
                 {
                     var pts = new PointF[]
@@ -32,7 +31,7 @@ namespace VizUtils
                         new PointF((float) (cell.Bounds.RaRight * pixelsPerDeg), (float) ((90.0f - cell.Bounds.DecBottom) * pixelsPerDeg)),
                         new PointF((float) (cell.Bounds.RaLeft * pixelsPerDeg), (float) ((90.0f - cell.Bounds.DecBottom) * pixelsPerDeg))
                     };
-                    ctx.DrawPolygon(brush, 2.0f, pts);
+                    ctx.DrawPolygon(Color.Black, 2.0f, pts);
                 }
             });
 
@@ -59,7 +58,6 @@ namespace VizUtils
         public static Image<Rgba32> DrawColoredSkySegmentCellOutline(this Image<Rgba32> baseImage, RaDecBounds bounds, Color color)
         {
             var pixelsPerDeg = baseImage.Width / 360;
-            var brush = new SolidBrush(color);
 
             var pts = new PointF[]
             {
@@ -68,7 +66,7 @@ namespace VizUtils
                 new PointF((float) (bounds.RaRight * pixelsPerDeg), (float) ((90.0f - bounds.DecBottom) * pixelsPerDeg)),
                 new PointF((float) (bounds.RaLeft * pixelsPerDeg), (float) ((90.0f - bounds.DecBottom) * pixelsPerDeg))
             };
-            baseImage.Mutate(context => context.DrawPolygon(brush, 2.0f, pts));
+            baseImage.Mutate(context => context.DrawPolygon(color, 2.0f, pts));
 
             return baseImage;
         }
@@ -77,13 +75,12 @@ namespace VizUtils
             double radiusRa, double radiusDec, Color color)
         {
             var pixelsPerDeg = baseImage.Width / 360;
-            var brush = new SolidBrush(color); //new Argb32(50, 50, 50, 100)
             float diameterRa = (float)(radiusRa * 2.0f);
             float diameterDec = (float)(radiusDec * 2.0f);
 
             var ellipse = new EllipsePolygon((float)(center.Ra * pixelsPerDeg), (float)((90.0f - center.Dec) * pixelsPerDeg),
                 (float)(diameterRa * pixelsPerDeg), (float)(diameterDec * pixelsPerDeg));
-            baseImage.Mutate(context => context.Fill(brush, ellipse));
+            baseImage.Mutate(context => context.Fill(color, ellipse));
 
             return baseImage;
         }
@@ -92,13 +89,12 @@ namespace VizUtils
             double radiusRa, double radiusDec, Color color, float thickness)
         {
             var pixelsPerDeg = baseImage.Width / 360;
-            var brush = new SolidBrush(color); //new Argb32(50, 50, 50, 100)
             float diameterRa = (float)(radiusRa * 2.0f);
             float diameterDec = (float)(radiusDec * 2.0f);
 
             var ellipse = new EllipsePolygon((float)(center.Ra * pixelsPerDeg), (float)((90.0f - center.Dec) * pixelsPerDeg),
                 (float)(diameterRa * pixelsPerDeg), (float)(diameterDec * pixelsPerDeg));
-            baseImage.Mutate(context => context.Draw(brush, thickness, ellipse));
+            baseImage.Mutate(context => context.Draw(color, thickness, ellipse));
 
             return baseImage;
         }
